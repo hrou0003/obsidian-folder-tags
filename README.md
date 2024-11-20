ec204127b93a599d0463396d93dba696402b6017
Harrison <hrouillard@sfi.com.au>
2024-11-20 20:52:09 +1100

    Semi-functional

--- README.md
+++ README.md
@@ -1,94 +1,65 @@
-# Obsidian Sample Plugin
+# Obsidian Folder Tags Plugin
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+This plugin allows you to add tags to folders in Obsidian and automatically apply those tags to files within those folders. Tags are inherited by subfolders and can be managed through the interface.
 
-This project uses TypeScript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in TypeScript Definition format, which contains TSDoc comments describing what it does.
+## Features
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+- Add tags to folders that automatically apply to all files within
+- Tags are inherited by subfolders and their contents
+- New files automatically receive folder tags when created
+- Edit or remove folder tags through the settings panel
+- Folder context menu integration for quick tag management
+- Support for both frontmatter and inline tags
+- Handles tag inheritance throughout the folder hierarchy
 
-## First time developing plugins?
+## Usage
 
-Quick starting guide for new plugin devs:
+### Adding Tags to a Folder
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+There are two ways to add tags to a folder:
 
-## Releasing new releases
+1. **Context Menu**
+   - Right-click on any folder
+   - Select "Add folder tags"
+   - Enter your tags separated by commas
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+2. **Command Palette**
+   - Open the command palette (Ctrl/Cmd + P)
+   - Search for "Add tags to current folder"
+   - Enter your tags separated by commas
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
+### Managing Tags
 
-## Adding your plugin to the community plugin list
+You can manage all folder tags through the plugin settings:
+1. Go to Settings > Plugin Options > Folder Tags
+2. Each folder with tags will be listed
+3. Use the "Edit" button to modify tags
+4. Use the "Remove" button to delete folder tags
 
-- Check the [plugin guidelines](https://docs.obsidian.md/Plugins/Releasing/Plugin+guidelines).
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
+## Installation
 
-## How to use
+1. Open Obsidian Settings
+2. Go to Community Plugins and turn off Restricted Mode
+3. Click Browse and search for "Folder Tags"
+4. Install the plugin
+5. Enable the plugin in your Community Plugins settings
 
-- Clone this repo.
-- Make sure your NodeJS is at least v16 (`node --version`).
-- `npm i` or `yarn` to install dependencies.
-- `npm run dev` to start compilation in watch mode.
+## Manual Installation
 
-## Manually installing the plugin
+1. Download `main.js`, `manifest.json`, and `styles.css` from the latest release
+2. Create a new folder in your vault: `.obsidian/plugins/obsidian-folder-tags/`
+3. Copy the downloaded files into this folder
+4. Reload Obsidian
+5. Enable the plugin in Community Plugins settings
 
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
+## Development
 
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
+- Clone this repository to `.obsidian/plugins/obsidian-folder-tags/`
+- `npm install` to install dependencies
+- `npm run dev` to start compilation in watch mode
+- Make changes to `main.ts`
+- Reload Obsidian to see changes
 
-## Funding URL
+## Support
 
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+If you find any bugs or have feature requests, please create an issue on the GitHub repository.
--- main.ts
+++ main.ts
@@ -1,134 +1,441 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TFolder,
+	TFile,
+	EventRef,
+	Modal,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface FolderTag {
+	path: string;
+	tags: string[];
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface FolderTagsSettings {
+	folderTags: FolderTag[];
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: FolderTagsSettings = {
+	folderTags: [],
+};
+
+export default class FolderTagsPlugin extends Plugin {
+	settings: FolderTagsSettings;
+	fileCreateRef: EventRef;
+	fileModifyRef: EventRef;
+	isApplyingTags = false;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// Add settings tab
+		this.addSettingTab(new FolderTagsSettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Add command to add tags to current folder
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "add-tags-to-folder",
+			name: "Add tags to current folder",
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
+				const activeFile = this.app.workspace.getActiveFile();
+				if (activeFile?.parent) {
+					this.addTagsToFolder(activeFile.parent);
+				}
+			},
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		// Add folder context menu item
+		this.registerEvent(
+			this.app.workspace.on("file-menu", (menu, file) => {
+				if (file instanceof TFolder) {
+					menu.addItem((item) => {
+						item.setTitle("Add folder tags")
+							.setIcon("tag")
+							.onClick(() => {
+								this.addTagsToFolder(file);
+							});
+					});
+				}
+			}),
+		);
+
+		// Monitor file creation only
+		this.fileCreateRef = this.app.vault.on("create", (file) => {
+			if (file instanceof TFile) {
+				// Only apply inherited tags to new files
+				const folderPath = file.parent?.path || "";
+				const inheritedTags = new Set(this.getFolderTags(folderPath));
+				if (inheritedTags.size > 0) {
+					this.addTagsToFileContent(file, inheritedTags);
 				}
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
+		this.app.vault.offref(this.fileCreateRef);
+		this.app.metadataCache.offref(this.fileModifyRef);
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData(),
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+		// Update all file tags when settings change
+	}
+
+	getFolderTags(folderPath: string): string[] {
+		// Get direct tags for this folder
+		const folderTag = this.settings.folderTags.find(
+			(ft) => ft.path === folderPath,
+		);
+		const directTags = folderTag ? folderTag.tags : [];
+
+		// Get inherited tags from parent folders
+		const parentTags = this.getInheritedTags(folderPath);
+
+		// Combine and deduplicate tags
+		return Array.from(new Set([...directTags, ...parentTags]));
+	}
+
+	getInheritedTags(path: string): string[] {
+		const pathParts = path.split("/");
+		let inheritedTags: string[] = [];
+		let currentPath = "";
+
+		// Build up path progressively to check each parent folder
+		for (const part of pathParts) {
+			if (currentPath) {
+				currentPath += "/";
+			}
+			currentPath += part;
+
+			const folderTag = this.settings.folderTags.find(
+				(ft) => ft.path === currentPath,
+			);
+			if (folderTag) {
+				inheritedTags = [...inheritedTags, ...folderTag.tags];
+			}
+		}
+
+		return inheritedTags;
+	}
+
+	async setFolderTags(folderPath: string, tags: string[]) {
+		const existingIndex = this.settings.folderTags.findIndex(
+			(ft) => ft.path === folderPath,
+		);
+
+		if (existingIndex >= 0) {
+			this.settings.folderTags[existingIndex].tags = tags;
+		} else {
+			this.settings.folderTags.push({
+				path: folderPath,
+				tags: tags,
+			});
+		}
+
+		await this.saveSettings();
+	}
+
+	private async addTagsToFileContent(file: TFile, newTags: Set<string>) {
+		if (newTags.size === 0) return;
+
+		const content = await this.app.vault.read(file);
+		const metadata = this.app.metadataCache.getFileCache(file);
+
+		// Get all existing tags in the file
+		const existingTags = new Set<string>();
+
+		// Get tags from frontmatter
+		if (metadata?.frontmatter?.tags) {
+			const fmTags = metadata.frontmatter.tags;
+			if (Array.isArray(fmTags)) {
+				fmTags.forEach((tag) => existingTags.add(tag));
+			} else {
+				existingTags.add(fmTags);
+			}
+		}
+
+		// Get inline tags
+		if (metadata?.tags) {
+			Object.keys(metadata.tags).forEach((tag) => {
+				existingTags.add(tag.substring(1));
+			});
+		}
+
+		// Filter out tags that already exist
+		const tagsToAdd = new Set(
+			[...newTags].filter((tag) => !existingTags.has(tag)),
+		);
+		if (tagsToAdd.size === 0) return;
+
+		// If frontmatter exists, add tags there
+		if (metadata?.frontmatter) {
+			const frontmatterRegex = /^---\n([\s\S]*?)\n---/;
+			const frontmatterMatch = content.match(frontmatterRegex);
+
+			if (frontmatterMatch) {
+				const yaml = this.parseYaml(frontmatterMatch[1]);
+				const existingTags = yaml.tags || [];
+				const allTags = Array.from(
+					new Set([
+						...(Array.isArray(existingTags)
+							? existingTags
+							: [existingTags]),
+						...newTags,
+					]),
+				);
+
+				yaml.tags = allTags;
+				const newFrontmatter = this.stringifyYaml(yaml);
+				const newContent = content.replace(
+					frontmatterRegex,
+					`---\n${newFrontmatter}---`,
+				);
+
+				console.log(newContent);
+				await this.app.vault.modify(file, newContent);
+			}
+		}
+		// Else add it to the bottom of the file
+		else {
+			const tagString = Array.from(newTags)
+				.map((tag) => `#${tag}`)
+				.join(" ");
+
+			const newContent = `${content.trimEnd()}\n\n${tagString}`;
+			console.log(newContent);
+			await this.app.vault.modify(file, newContent);
+		}
+	}
+
+	private async removeTagsFromFileContent(
+		file: TFile,
+		tagsForRemoval: Set<string>,
+	) {
+		if (tagsForRemoval.size === 0) return;
+
+		const content = await this.app.vault.read(file);
+		const metadata = this.app.metadataCache.getFileCache(file);
+		let newContent = content;
+
+		// Remove from frontmatter if it exists
+		if (metadata?.frontmatter) {
+			const frontmatterRegex = /^---\n([\s\S]*?)\n---/;
+			const frontmatterMatch = content.match(frontmatterRegex);
+
+			if (frontmatterMatch) {
+				const yaml = this.parseYaml(frontmatterMatch[1]);
+				if (yaml.tags) {
+					const existingTags = Array.isArray(yaml.tags)
+						? yaml.tags
+						: [yaml.tags];
+					yaml.tags = existingTags.filter(
+						(tag: string) => !tagsForRemoval.has(tag),
+					);
+
+					// If tags array is empty, remove the tags property
+					if (yaml.tags.length === 0) {
+						delete yaml.tags;
+					}
+
+					const newFrontmatter = this.stringifyYaml(yaml);
+					newContent = content.replace(
+						frontmatterRegex,
+						`---\n${newFrontmatter}---`,
+					);
+				}
+			}
+		}
+
+		// Remove inline tags
+		const tagsToRemovePattern = Array.from(tagsForRemoval)
+			.map((tag) => `#${tag}`)
+			.join("|");
+		if (tagsToRemovePattern) {
+			const tagRegex = new RegExp(`\\s*(${tagsToRemovePattern})\\b`, "g");
+			newContent = newContent.replace(tagRegex, " "); // Replace with a single space instead of empty string
+		}
+
+		if (newContent !== content) {
+			console.log(newContent);
+			await this.app.vault.modify(file, newContent);
+		}
+	}
+
+	private parseYaml(yaml: string): any {
+		try {
+			// Simple YAML parser for frontmatter
+			const result: any = {};
+			const lines = yaml.split("\n");
+			for (const line of lines) {
+				const [key, ...values] = line.split(":").map((s) => s.trim());
+				if (key && values.length) {
+					const value = values.join(":").trim();
+					if (value.startsWith("[") && value.endsWith("]")) {
+						result[key] = value
+							.slice(1, -1)
+							.split(",")
+							.map((s) => s.trim());
+					} else {
+						result[key] = value;
+					}
+				}
+			}
+			return result;
+		} catch (e) {
+			console.error("Error parsing YAML:", e);
+			return {};
+		}
+	}
+
+	private stringifyYaml(obj: any): string {
+		// Simple YAML stringifier for frontmatter
+		return Object.entries(obj)
+			.map(([key, value]) => {
+				if (Array.isArray(value)) {
+					return `${key}: [${value.join(", ")}]`;
+				}
+				return `${key}: ${value}`;
+			})
+			.join("\n");
+	}
+
+	async addTagsToFolder(folder: TFolder) {
+		const existingTags = this.getFolderTags(folder.path);
+		const modal = new TagInputModal(
+			this.app,
+			existingTags,
+			async (newTags) => {
+				// Find tags that were removed
+				const removedTags = new Set(
+					existingTags.filter((tag) => !newTags.includes(tag)),
+				);
+
+				// Find tags that were added
+				const addedTags = new Set(
+					newTags.filter((tag) => !existingTags.includes(tag)),
+				);
+
+				// Update folder settings
+				await this.setFolderTags(folder.path, newTags);
+
+				// Update all files in the folder and subfolders
+				const files = this.app.vault
+					.getFiles()
+					.filter((file) => file.path.startsWith(folder.path));
+
+				for (const file of files) {
+					if (removedTags.size > 0) {
+						await this.removeTagsFromFileContent(file, removedTags);
+					}
+					if (addedTags.size > 0) {
+						await this.addTagsToFileContent(file, addedTags);
+					}
+				}
+			},
+		);
+		modal.open();
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class TagInputModal extends Modal {
+	tags: string[];
+	onSubmit: (tags: string[]) => void;
+
+	constructor(
+		app: App,
+		existingTags: string[],
+		onSubmit: (tags: string[]) => void,
+	) {
 		super(app);
+		this.tags = existingTags;
+		this.onSubmit = onSubmit;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+
+		contentEl.createEl("h2", { text: "Enter folder tags" });
+
+		const inputEl = contentEl.createEl("input", {
+			type: "text",
+			value: this.tags.join(", "),
+		});
+
+		const buttonEl = contentEl.createEl("button", {
+			text: "Save",
+		});
+
+		buttonEl.onclick = () => {
+			const newTags = inputEl.value
+				.split(",")
+				.map((tag) => tag.trim())
+				.filter((tag) => tag.length > 0);
+			this.onSubmit(newTags);
+			this.close();
+		};
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class FolderTagsSettingTab extends PluginSettingTab {
+	plugin: FolderTagsPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: FolderTagsPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		containerEl.createEl("h2", { text: "Folder Tags Settings" });
+
+		this.plugin.settings.folderTags.forEach((folderTag) => {
+			new Setting(containerEl)
+				.setName(folderTag.path)
+				.setDesc(`Tags: ${folderTag.tags.join(", ")}`)
+				.addButton((button) =>
+					button.setButtonText("Edit").onClick(async () => {
+						const modal = new TagInputModal(
+							this.app,
+							folderTag.tags,
+							async (newTags) => {
+								await this.plugin.setFolderTags(
+									folderTag.path,
+									newTags,
+								);
+								this.display();
+							},
+						);
+						modal.open();
+					}),
+				)
+				.addButton((button) =>
+					button.setButtonText("Remove").onClick(async () => {
+						this.plugin.settings.folderTags =
+							this.plugin.settings.folderTags.filter(
+								(ft) => ft.path !== folderTag.path,
+							);
+						await this.plugin.saveSettings();
+						this.display();
+					}),
+				);
+		});
 	}
 }
